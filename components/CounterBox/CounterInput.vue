62ab9771cac19d6d250d4b429c78e7d056ec8963
nimatorabi <46302001+nimatorabi@users.noreply.github.com>
2021-03-05 19:24:36 +0330

    create model for add counter step to counters list

--- components/CounterBox/CounterInput.vue
+++ components/CounterBox/CounterInput.vue
@@ -1,9 +1,9 @@
 <template>
 <div class="px-4">
     <b-input-group >
-        <b-form-input placeholder="step" type="number" />
+        <b-form-input placeholder="step" v-model="counterStep" type="number" />
         <b-input-group-append>
-            <b-button variant="dark">Add Counter</b-button>
+            <b-button variant="dark" @click="addCounter">Add Counter</b-button>
         </b-input-group-append>
     </b-input-group>
     <span class="text-muted pl-2">this only accepts numbers like 1,2,-5,28,...</span>
@@ -12,7 +12,11 @@
 
 <script>
 export default {
-
+    data(){
+        return{
+            counterStep:undefined
+        }
+    }
 }
 </script>
 
---
+++ store/counter.js
@@ -0,0 +1,7 @@
+export const state = {
+    counters: [1, 2, 5]
+}
+
+export const mutation = {
+
+}
