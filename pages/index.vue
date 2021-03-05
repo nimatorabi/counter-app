fd95df94d380b0d94ff3ac9db5185431a7e53ebd
nimatorabi <46302001+nimatorabi@users.noreply.github.com>
2021-03-05 19:06:03 +0330

    start to create counterBox

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,19 @@
 <template>
-  <div>
-    Hello world!
-  </div>
+<b-row>
+    <b-container>
+        <b-col cols="12">
+            <counter-box />
+        </b-col>
+    </b-container>
+</b-row>
 </template>
 
 <script>
+import CounterBox from "~/components/CounterBox/index.vue"
 export default {
-
+    components: {
+        CounterBox
+    }
 }
 </script>
 
