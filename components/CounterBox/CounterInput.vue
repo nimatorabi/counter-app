0bff67ab9eec93df9d680acc5f05e046a0d1f8e1
nimatorabi <46302001+nimatorabi@users.noreply.github.com>
2021-03-05 19:13:24 +0330

    counter input completed

--- components/CounterBox/CounterInput.vue
+++ components/CounterBox/CounterInput.vue
@@ -1,9 +1,11 @@
 <template>
-  <div>
-      counter input
-  </div>
+<b-input-group class="mt-3">
+    <b-form-input placeholder="step"/>
+    <b-input-group-append>
+        <b-button variant="info">Add Counter</b-button>
+    </b-input-group-append>
+</b-input-group>
 </template>
-
 <script>
 export default {
 
