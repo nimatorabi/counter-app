7ba992eea7f08ef49e9dabbcf849ab6554ebac4e
nimatorabi <46302001+nimatorabi@users.noreply.github.com>
2021-03-05 19:57:12 +0330

    create card box on store counter len

---
+++ components/CardBox/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="bg-white shadow-md rounded border border-dark mb-4">
+      <div>
+          <p>
+              step : {{counterStep}}
+          </p>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    props:{
+        counterStep:{
+            type:Number,
+            required:true
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
